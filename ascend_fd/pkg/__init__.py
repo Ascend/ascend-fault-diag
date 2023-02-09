97195445aa99bbe5a78a2faf41cbbca618a95351
nauyll <ly_luo1204@163.com>
2023-02-09 14:34:35 +0800

    【蓝区合入】330: 首节点清洗与分析模块

---
+++ ascend_fd/pkg/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+# Copyright(C) Huawei Technologies Co.,Ltd. 2022. ALL rights reserved.
+from ascend_fd.pkg.rc_diag import start_rc_diag_job
+from ascend_fd.pkg.kg_diag import start_kg_diag_job
+
+from ascend_fd.pkg.rc_parse import start_rc_parse_job
+from ascend_fd.pkg.kg_parse import start_kg_parse_job
---
+++ ascend_fd/pkg/rc_diag/__init__.py
@@ -0,0 +1,3 @@
+# coding: UTF-8
+# Copyright (c) 2022. Huawei Technologies Co., Ltd. ALL rights reserved.
+from ascend_fd.pkg.rc_diag.rc_diag_job import start_rc_diag_job
