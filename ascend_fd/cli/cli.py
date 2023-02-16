a6a6ff22bd3bf773d08eed24a431cd813811a9a0
tands7 <765339608@qq.com>
2023-02-16 11:15:43 +0800

    【蓝区合入】 330，优化命令行帮助信息回显

--- ascend_fd/cli/cli.py
+++ ascend_fd/cli/cli.py
@@ -27,22 +27,22 @@ def command_line():
     sub_arg.add_parser("version", description="show ascend-fd version", help="show ascend-fd version")
 
     parse_cmd = sub_arg.add_parser("parse", help="parse origin log files")
-    parse_cmd.add_argument("-i", "--input_path", type=str, required=True,
+    parse_cmd.add_argument("-i", "--input_path", type=str, required=True, metavar='',
                            help="the input path of origin data file")
-    parse_cmd.add_argument("-o", "--output_path", type=str, required=True,
+    parse_cmd.add_argument("-o", "--output_path", type=str, required=True, metavar='',
                            help="the output path of parsed data file")
 
     diag_cmd = sub_arg.add_parser("diag", help="diag parsed log files")
-    diag_cmd.add_argument("-i", "--input_path", type=str, required=True,
+    diag_cmd.add_argument("-i", "--input_path", type=str, required=True, metavar='',
                           help="the input path of parsed data file")
-    diag_cmd.add_argument("-o", "--output_path", type=str, required=True,
+    diag_cmd.add_argument("-o", "--output_path", type=str, required=True, metavar='',
                           help="the output path of diag result file")
-    diag_cmd.add_argument("-m", "--mode", type=int, default=0, choices=[0, 1],
+    diag_cmd.add_argument("-m", "--mode", type=int, default=0, choices=[0, 1], metavar='',
                           help="indicate whether a force-kill scenario is used. "
-                               "0: force-kill, 1: not force-kill. default 0.")
+                               "0: force-kill, 1: not force-kill. default 0")
     diag_cmd.add_argument("-p", "--print", action="store_true",
                           help="indicate whether to print the result. "
-                               "If no parameter is specified, the result is not printed.")
+                               "If no parameter is specified, the result is not printed")
 
     return args.parse_args()
 
